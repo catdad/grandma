70a1611d03a1aeb6794d55ba85825fa44985fd37
Kiril Vatev <vatev.1@gmail.com>
2016-03-22 22:00:23 -0400

    more realistic test using a web request
    will improve later, using a local file for now

--- fixtures/one.js
+++ fixtures/one.js
@@ -1,16 +1,34 @@
 /* jshint node: true */
 
-var loaded = Date.now();
+//var loaded = Date.now();
+//
+//module.exports = {
+//    beforeEach: function(done) {
+//        process.nextTick(done);
+//    },
+//    test: function(done) {
+//        console.log('test', Date.now() - loaded);
+//        process.nextTick(done);
+//    },
+//    afterEach: function(done) {
+//        process.nextTick(done);
+//    }
+//};
+
+var http = require('http');
 
 module.exports = {
-    beforeEach: function(done) {
-        process.nextTick(done);
-    },
     test: function(done) {
-        console.log('test', Date.now() - loaded);
-        process.nextTick(done);
-    },
-    afterEach: function(done) {
-        process.nextTick(done);
+        var req = http.request({
+            method: 'GET',
+            host: 'localhost',
+            path: '/share.txt'
+        }, function(res) {
+            res.on('data', function() {});
+            res.on('end', done);
+            res.on('error', done);
+        });
+        
+        req.end();
     }
 };
