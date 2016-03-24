30ff3b21145d385b87ff59661e3586cf7f35d666
Kiril Vatev <vatev.1@gmail.com>
2016-03-23 20:43:20 -0400

    separating fixtures

--- fixtures/one.js
+++ fixtures/one.js
@@ -1,34 +1,16 @@
 /* jshint node: true */
 
-//var loaded = Date.now();
-//
-//module.exports = {
-//    beforeEach: function(done) {
-//        process.nextTick(done);
-//    },
-//    test: function(done) {
-//        console.log('test', Date.now() - loaded);
-//        process.nextTick(done);
-//    },
-//    afterEach: function(done) {
-//        process.nextTick(done);
-//    }
-//};
-
-var http = require('http');
+var loaded = Date.now();
 
 module.exports = {
+    beforeEach: function(done) {
+        process.nextTick(done);
+    },
     test: function(done) {
-        var req = http.request({
-            method: 'GET',
-            host: 'localhost',
-            path: '/share.txt'
-        }, function(res) {
-            res.on('data', function() {});
-            res.on('end', done);
-            res.on('error', done);
-        });
-        
-        req.end();
+        console.log('test', Date.now() - loaded);
+        process.nextTick(done);
+    },
+    afterEach: function(done) {
+        process.nextTick(done);
     }
 };
---
+++ fixtures/req.js
@@ -0,0 +1,19 @@
+/* jshint node: true */
+
+var http = require('http');
+
+module.exports = {
+    test: function(done) {
+        var req = http.request({
+            method: 'GET',
+            host: 'localhost',
+            path: '/share.txt'
+        }, function(res) {
+            res.on('data', function() {});
+            res.on('end', done);
+            res.on('error', done);
+        });
+        
+        req.end();
+    }
+};
