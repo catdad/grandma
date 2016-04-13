5a304907e24f6d0bb321ad6f0a11daaedaa62c90
Kiril Vatev <vatev.1@gmail.com>
2016-04-12 22:45:10 -0400

    adding lib/stats.js tests stub
    https://github.com/catdad/grandma/issues/9

--- test/stats.test.js
+++ test/stats.test.js
@@ -1,3 +1,10 @@
 /* jshint node: true, mocha: true, expr: true */
 
+var expect = require('chai').expect;
 var stats = require('../lib/stats.js');
+
+describe('[stats]', function() {
+    it('has all expected keys', function() {
+        expect(stats).to.have.all.keys(['mean', 'median', 'percentile']);
+    });
+});
