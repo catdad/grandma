af1cedfa592172428c0d2baeafd7bf1291b26959
Kiril Vatev <vatev.1@gmail.com>
2016-04-12 22:49:02 -0400

    more tests
    https://github.com/catdad/grandma/issues/9

--- test/stats.test.js
+++ test/stats.test.js
@@ -7,4 +7,24 @@ describe('[stats]', function() {
     it('has all expected keys', function() {
         expect(stats).to.have.all.keys(['mean', 'median', 'percentile']);
     });
+    
+    describe('#mean', function() {
+        it('calculated averages', function() {
+            expect(stats.mean([1, 2, 3])).to.equal(2);
+        });
+        
+        [
+            null,
+            1,
+            {},
+            function() {}
+        ].forEach(function(val) {
+            it('throws if given "' + (JSON.stringify(val) || val.toString()) + '"', function() {
+                this.skip();
+            });
+        });
+    });
+    
+    describe('#median', function() {});
+    describe('#percentile', function() {});
 });
