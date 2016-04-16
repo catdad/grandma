67380622236d17a46e198883f9c0e82db4513063
Kiril Vatev <vatev.1@gmail.com>
2016-04-15 21:15:49 -0400

    adding a logger module

---
+++ test/logger.test.js
@@ -0,0 +1,116 @@
+/* jshint node: true, mocha: true, expr: true */
+
+var util = require('util');
+
+var expect = require('chai').expect;
+var _ = require('lodash');
+
+var Logger = require('../lib/logger.js');
+
+function mockStream(onWrite) {
+    return {
+        write: onWrite
+    };
+}
+
+describe('[logger]', function() {
+    
+    function testLoggingMethod(options) {
+        var name = options.name;
+        var streamName = options.streamName;
+        var optName = options.optName;
+        var envName = options.envName;
+        
+        describe('#' + name, function() {
+            function testLog(opts, args, expectedData) {
+                var stream = mockStream(function(data) {
+                    expect(data).to.equal(expectedData + '\n');
+                });
+                
+                var testOpts = {};
+                testOpts[streamName] = stream;
+
+                opts = _.defaultsDeep(opts, testOpts);
+
+                var logger = Logger(opts);
+                logger[name].apply(logger, args);
+            }
+            
+            it('writes to stdout alternate stream', function() {
+                var DATA = 'monkeys';
+                
+                var opts = {};
+                opts[optName] = true;
+
+                testLog(opts, [DATA], DATA);
+            });
+
+            it('formats strings', function() {
+                var DATA = 'monkeys';
+
+                var opts = {};
+                opts[optName] = true;
+                
+                testLog(opts, ['not %s', DATA], 'not ' + DATA);
+            });
+
+            it('can be enabled with an env variable', function() {
+                var DATA = 'monkeys';
+
+                process.env[envName] = 1;
+
+                testLog({}, [DATA], DATA);
+
+                delete process.env[envName];
+            });
+
+            it('queues writes synchronously', function() {
+                var wrote = false;
+
+                var stream = mockStream(function() {
+                    wrote = true;
+                });
+
+                var opts = {};
+                opts[optName] = true;
+                opts[streamName] = stream;
+                
+                var logger = Logger(opts);
+
+                expect(wrote).to.equal(false);
+                logger[name]('flapjacks');
+                expect(wrote).to.equal(true);
+            });
+
+            it('does not write when not enabled', function() {
+                var stream = mockStream(function() {
+                    throw new Error('should not write to stream');
+                });
+
+                var opts = {};
+                opts[streamName] = stream;
+                
+                var logger = Logger(opts);
+
+                logger[name]('flapjacks');
+            });
+            
+        });
+        
+    }
+    
+    testLoggingMethod({
+        name: 'log',
+        streamName: 'stdout',
+        optName: 'debugLog',
+        envName: 'GRANDMA_DEBUG_LOG'
+    });
+    
+    testLoggingMethod({
+        name: 'error',
+        streamName: 'stderr',
+        optName: 'debugError',
+        envName: 'GRANDMA_DEBUG_ERROR'
+    });
+    
+});
