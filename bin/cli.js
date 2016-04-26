e081b9df7d2a0e49f87ca86eb63d044206a41ec9
Kiril Vatev <vatev.1@gmail.com>
2016-04-25 22:05:32 -0400

    cleanup, comments

--- bin/cli.js
+++ bin/cli.js
@@ -11,7 +11,7 @@ function requireLocal(moduleName) {
     } catch(e) {
         return require(moduleName);
     }
-};
+}
 
 function requireGrandma(from) {
     try {
@@ -25,10 +25,13 @@ function requireGrandma(from) {
 }
 
 if (!requireGrandma('grandma')) {
-  if (!requireGrandma('./')) {
-    // We did not find grandma installed locally, nor are we
-    // executing in the grandma folder itself (which might happen
-    // during development), so we will use the global one instead.
-    requireGrandma('../index');
-  }
+    // This is requiring the package at the current root (cwd), assuming
+    // that the user is me actually developing grandma. This seems like
+    // a bad idea, but it is currently helpful... I think.
+    if (!requireGrandma('./')) {
+        // We did not find grandma installed locally, nor are we
+        // executing in the grandma folder itself (which might happen
+        // during development), so we will use the global one instead.
+        requireGrandma('../index');
+    }
 }
