13b1eaa456fb703d4d931eff0a1aafb30b8a40cd
Kiril Vatev <vatev.1@gmail.com>
2016-05-04 21:06:06 -0400

    adding enums tests, cleanup

---
+++ test/enums.test.js
@@ -0,0 +1,52 @@
+/* jshint node: true, mocha: true, expr: true */
+
+var _ = require('lodash');
+var Enum = require('../lib/enums.js');
+
+var expect = require('chai').expect;
+
+describe('[enums]', function() {
+    var ENUM = {
+        apple: 'fruit',
+        carrot: 'vegetable'
+    };
+        
+    it('creates an enums object', function() {
+        var e = Enum(ENUM);
+        
+        expect(e).to.be.an('object');
+        expect(e).to.have.all.keys(_.keys(ENUM));
+        expect(e).to.not.equal(ENUM);
+    });
+    
+    it('can be used to get values', function() {
+        var e = Enum(ENUM);
+        
+        _.forEach(ENUM, function(val, key) {
+            expect(e).to.have.property(key).and.to.equal(val);
+        });
+    });
+    
+    describe('#fromValue', function() {
+        it('is a non-enumerable method', function() {
+            var e = Enum(ENUM);
+            
+            expect(e).to.have.property('fromValue').and.to.be.a('function');
+            expect(Object.keys(e)).to.not.contain('fromValue');
+        });
+        
+        it('returns the key using a value', function() {
+            var e = Enum(ENUM);
+            
+            _.forEach(function(val, key) {
+                expect(e.fromValue(val)).to.equal(key);
+            });
+        });
+        
+        it('returns undefined if the value does not exist', function() {
+            var e = Enum(ENUM);
+            
+            expect(e.fromValue('apple' + Math.random())).to.equal(void 0);
+        });
+    });
+});
