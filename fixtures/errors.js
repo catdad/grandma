4cda2d4f1a53ddf72f00dcc9a5f9ce4a272f4ded
Kiril Vatev <vatev.1@gmail.com>
2016-05-15 13:25:07 -0400

    adding error test fixture

---
+++ fixtures/errors.js
@@ -0,0 +1,16 @@
+/* jshint node: true */
+
+module.exports = {
+    test: function(done) {
+        setTimeout(function() {
+            var err;
+            
+            if (Date.now() % 2) {
+                err = new Error('a bad thing happened');
+                err.errorCode = 123;
+            }
+            
+            done(err);
+        }, 1);
+    },
+};
