a335f4e5b6db5b5a264dc39fe8303e023240bc24
Kiril Vatev <vatev.1@gmail.com>
2016-07-26 20:44:38 -0400

    fixing a but in run-utils with writing to the raw output instead of the wrapped stream

--- test/run-utils.test.js
+++ test/run-utils.test.js
@@ -1,8 +1,32 @@
 /* eslint-env mocha */
 
-describe('[run-utils]', function() {
+var expect = require('chai').expect;
+var through = require('through2');
+
+var runUtils = require('../lib/run-utils.js');
+
+describe.only('[run-utils]', function() {
+    it('wraps the output stream and returns a new stream in the exports', function() {
+        var a = through();
+        
+        var util = runUtils({
+            output: a
+        });
+        
+        expect(util.output)
+            .and.to.have.property('pipe')
+            .and.to.be.a('function');
+        expect(util.output).to.not.equal(a);
+    });
+    
     describe('#wrapStream', function() {
-        it('returns a new stream');
+        it.skip('returns a new stream', function(done) {
+            var input = through();
+            
+            runUtils({
+                output: input
+            });
+        });
         it('returns an object stream when objectMode is set on utils');
         it('can gzip data');
         it('triggers the "fullEnd" event when the data ends');
