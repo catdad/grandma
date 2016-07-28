096a0d554ee2be1684d5435bd75e85df3d7ddfda
Kiril Vatev <vatev.1@gmail.com>
2016-07-27 20:00:08 -0400

    adding tests for setting rate and concurrent at runtime
    #69

--- test/run.test.js
+++ test/run.test.js
@@ -453,4 +453,131 @@ describe('[run]', function() {
         });
     }());
     
+    describe('interactive mode:', function() {
+        
+        var CONCURRENT_TEST = {
+            path: path.resolve(__dirname, '../fixtures/test.concurrent.js'),
+            name: 'test.concurrent'
+        };
+        
+        var RATE_TEST = {
+            path: path.resolve(__dirname, '../fixtures/test.small.js'),
+            name: 'test.small'
+        };
+        
+        it('can have concurrency changed at runtime when running in concurrent mode', function(done) {
+            increaseTimeout(this);
+
+            var INIT_C = 1;
+            var FINAL_C = 20;
+            var count = 0;
+
+            var output = through.obj();
+
+            var opts = {
+                duration: 50,
+                concurrent: INIT_C,
+                test: CONCURRENT_TEST,
+                output: output
+            };
+
+            var increateConcurrent = _.once(function() {
+                task.concurrent = FINAL_C;
+            });
+
+            output.on('data', function(data) {
+                if (data.report) {
+                    count += 1;
+                    increateConcurrent();
+                }
+            });
+
+            var task = run(opts, function(err) {
+                expect(count).to.be.at.least(FINAL_C);
+                expect(task).to.have.property('concurrent')
+                    .and.to.equal(FINAL_C);
+                done();
+            });
+
+            expect(task).to.have.property('concurrent')
+                .and.to.equal(INIT_C);
+        });
+
+        it('can have rate changed at runtime when running in rate mode', function(done) {
+            increaseTimeout(this);
+
+            var count = 0;
+
+            var output = through.obj();
+            var INIT_RATE = 1000 / 10 * 2;
+            var FINAL_RATE = 17000;
+
+            var opts = {
+                // we expect this to execute exactly 3 times
+                // without changing the rate at runtime
+                duration: '15ms',
+                rate: INIT_RATE,
+                test: RATE_TEST,
+                output: output
+            };
+
+            var increateConcurrent = _.once(function() {
+                // a very large number, since we are running
+                // the test for a very short time
+                task.rate = FINAL_RATE;
+            });
+
+            output.on('data', function(data) {
+                if (data.report) {
+                    count += 1;
+                    increateConcurrent();
+                }
+            });
+
+            var task = run(opts, function(err) {
+                // rate is less scientific, so just make
+                // sure it's more than the small amount previous
+                // tests got
+                expect(count).to.be.at.least(10);
+                expect(task).to.have.property('rate')
+                    .and.to.equal(FINAL_RATE);
+                done();
+            });
+            
+            expect(task).to.have.property('rate')
+                .and.to.equal(INIT_RATE);
+        });
+
+        it('throws if the runtime concurrent value is set to a non-integer', function() {
+            var output = through.obj();
+
+            var task = run({
+                duration: 50,
+                concurrent: 1,
+                test: CONCURRENT_TEST,
+                output: output
+            }, _.noop);
+
+            expect(function() {
+                task.concurrent = 3.14;
+            }).to.throw(TypeError, 'concurrent must be a positive non-zero integer');
+        });
+
+        it('throws if the runtime rate value is set to a non-number', function() {
+            var output = through.obj();
+
+            var task = run({
+                duration: '10ms',
+                rate: 2000,
+                test: RATE_TEST,
+                output: output
+            }, _.noop);
+            
+            expect(function() {
+                task.rate = 'pineapples';
+            }).to.throw(TypeError, 'rate must be a positive number');
+        });
+
+    });
+    
 });
