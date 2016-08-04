2150f6f0c9095dc26b7a4b4b975993ea4405bd9d
Kiril Vatev <vatev.1@gmail.com>
2016-08-04 19:03:22 -0400

    reducing complex fractions to improve rate mode with small rates
    #79

--- test/run-rate-values.test.js
+++ test/run-rate-values.test.js
@@ -2,13 +2,28 @@
 /* eslint-disable no-unused-expressions, max-len, max-nested-callbacks, max-params */
 
 var expect = require('chai').expect;
+var _ = require('lodash');
 
 var rrv = require('../lib/run-rate-values.js');
 
 describe('[run-rate-values]', function() {
-    it('returns "rate", "intTime", and "concurrent" values', function() {
+    it('returns an object with the expected properties', function() {
         expect(rrv(1)).to.have.all.keys([
-            'rate', 'intTime', 'concurrent'
+            'rate', 'intTime', 'concurrent', 'realRate'
         ]);
+    });
+    
+    _.map(new Array(20), function(val, idx) {
+        return (61 + (idx * 2)) / 60;
+    }).forEach(function(val) {
+        it('estimates a close-enough rate for small value: ' + val, function() {
+            var estimate = rrv(val);
+            expect(Math.abs(estimate.realRate - val)).to.be.below(1);
+        });
+    });
+    
+    it('returns a rate of 1 per second for a value less than 1', function() {
+        var estimate = rrv(.5);
+        expect(estimate.realRate).to.equal(1);
     });
 });
