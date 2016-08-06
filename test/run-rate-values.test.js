c1d5e89b00d04ce314075a223a5e665dd95d9584
Kiril Vatev <vatev.1@gmail.com>
2016-08-05 22:49:16 -0400

    getting rate adjustments even closer to the real rate, more tests
    #79

--- test/run-rate-values.test.js
+++ test/run-rate-values.test.js
@@ -6,6 +6,8 @@ var _ = require('lodash');
 
 var rrv = require('../lib/run-rate-values.js');
 
+var DELTA_LIMIT = 0.1;
+
 describe('[run-rate-values]', function() {
     it('returns an object with the expected properties', function() {
         expect(rrv(1)).to.have.all.keys([
@@ -13,34 +15,43 @@ describe('[run-rate-values]', function() {
         ]);
     });
     
+    function expectAdjustment(rate, estimate) {
+        if (estimate.realRate > rate) {
+            // for small decimals, the rate will always be
+            // larger
+            expect(Math.abs(estimate.realRate - rate)).to.be.below(DELTA_LIMIT);
+        } else if (estimate.realRate < rate) {
+            // for larger numbers, we might dip a bit below
+            // in which case, we will use a bit more
+            // wiggle room
+            expect(Math.abs(estimate.realRate - rate)).to.be.below(1);
+        }
+    }
+    
     _.map(new Array(20), function(val, idx) {
         return (61 + (idx * 2)) / 60;
     }).concat([2.56, 3.14]).forEach(function(val) {
         it('estimates a close-enough rate for small value: ' + val, function() {
             var estimate = rrv(val);
-            expect(Math.abs(estimate.realRate - val)).to.be.below(1);
+            expectAdjustment(val, estimate);
         });
     });
     
-//    _.map(new Array(10), function(val, idx) {
-//        return (idx + 1) * 9 / 99;
-//    }).forEach(function(val) {
-//        it('estimates decimals less than 1 of value: ' + val, function() {
-//            var estimate = rrv(val);
-//            console.log(val, estimate);
-//
-//            expect(Math.abs(estimate.realRate - val)).to.be.below(1);
-//        });
-//    });
-//
-//    _.map(new Array(10), function(val, idx) {
-//        return (idx + 1) * 9 / 10;
-//    }).forEach(function(val) {
-//        it('estimates larger decimals of value: ' + val, function() {
-//            var estimate = rrv(val);
-//            console.log(val, estimate);
-//
-//            expect(Math.abs(estimate.realRate - val)).to.be.below(1);
-//        });
-//    });
+    _.map(new Array(10), function(val, idx) {
+        return (idx + 1) * 9 / 99;
+    }).forEach(function(val) {
+        it('estimates a decimal less than 1 of value: ' + val, function() {
+            var estimate = rrv(val);
+            expectAdjustment(val, estimate);
+        });
+    });
+
+    _.map(new Array(30), function(val, idx) {
+        return (idx + 1) * 9 / 10;
+    }).forEach(function(val) {
+        it('estimates larger decimal of value: ' + val, function() {
+            var estimate = rrv(val);
+            expectAdjustment(val, estimate);
+        });
+    });
 });
