ff651f7f5307ddedd27d87ef6a1d70a4c7d1bd11
Kiril Vatev <vatev.1@gmail.com>
2016-08-07 10:32:44 -0400

    bad socket ipc implementation
    still has bugs -- don't use

--- fixtures/req.js
+++ fixtures/req.js
@@ -2,15 +2,35 @@ var http = require('http');
 
 module.exports = {
     test: function(done) {
+//        console.log('making request');
+        
         var req = http.request({
             method: 'GET',
             host: 'localhost',
             path: '/share.txt'
         }, function(res) {
+//            console.log('got response');
+            
             res.on('data', function() {});
-            res.on('end', done);
-            res.on('error', done);
+            res.on('end', function() {
+//                console.log('end event');
+                done();
+            });
+            res.on('error', function(err) {
+//                console.log('err event');
+                done(err);
+            });
+            
+            res.on('close', function() {
+//                console.log('close event');
+            });
         });
+        
+//        req.on('error', function(err) {
+//            console.log('req err event');
+//        });
+        
+        req.setNoDelay(true);
         
         req.end();
     }
