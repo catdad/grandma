91012bedb56f07b3fac2478214979151d0013bb8
Kiril Vatev <vatev.1@gmail.com>
2016-08-07 16:56:55 -0400

    removing the console statement from the one.js ficture

--- fixtures/one.js
+++ fixtures/one.js
@@ -1,11 +1,11 @@
-var loaded = Date.now();
+// var loaded = Date.now();
 
 module.exports = {
     beforeEach: function(done) {
         process.nextTick(done);
     },
     test: function(done) {
-        console.log('test', Date.now() - loaded);
+//        console.log('test', Date.now() - loaded);
         process.nextTick(done);
     },
     afterEach: function(done) {
