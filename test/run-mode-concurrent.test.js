02c6ed057db91afba1cc5c663ca392a81466818b
Kiril Vatev <vatev.1@gmail.com>
2016-08-08 19:31:12 -0400

    adding some test stubs
    #82

---
+++ test/run-mode-concurrent.test.js
@@ -0,0 +1,10 @@
+/* eslint-env mocha */
+/* eslint-disable no-unused-expressions, max-len, max-nested-callbacks */
+
+describe('[run-mode-concurrent]', function() {
+    it('starts a defined number of concurrent tests immediately');
+    
+    it('starts a new test when the tick event fires');
+    
+    it('waits for all tests to finish running when the duration is reached');
+});
