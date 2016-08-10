628a2b68217f8a1fe9e41caaec642b3e96a67dc9
Kiril Vatev <vatev.1@gmail.com>
2016-08-10 19:04:26 -0400

    adding rate mode tests stubs
    #82

---
+++ test/run-mode-rate.test.js
@@ -0,0 +1,8 @@
+/* eslint-env mocha */
+/* eslint-disable no-unused-expressions, max-len, max-nested-callbacks */
+
+describe('[run-mode-rate]', function() {
+    it('starts tests at an internal, even if they are not finishing');
+    
+    it('stops running tests after a defined duration');
+});
