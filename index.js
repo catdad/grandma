0cdac3c8c4384803c94ea31d46b5edd21182ec41
Kiril Vatev <vatev.1@gmail.com>
2016-10-15 12:56:40 -0400

    adding #diff to the public API

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ var path = require('path');
 
 var run = require('./lib/run');
 var report = require('./lib/report');
+var diff = require('./lib/report-diff');
 
 module.exports = {
     run: run,
     report: report,
+    diff: diff,
     cliPath: function() {
         return path.resolve(__dirname, 'lib/cli');
     }
