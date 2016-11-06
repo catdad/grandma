5aabacbcd3ce71061763f6034ce60000fe8d405f
Kiril Vatev <vatev.1@gmail.com>
2016-11-06 11:12:18 -0500

    extracting grandma cli helper to a util

--- test/cli/report.test.js
+++ test/cli/report.test.js
@@ -1,13 +1,9 @@
 /* eslint-env mocha */
 /* eslint-disable no-unused-expressions, max-nested-callbacks, max-len */
 
-var path = require('path');
-var util = require('util');
-
 var expect = require('chai').expect;
-var shellton = require('shellton');
-var through = require('through2');
-var root = require('rootrequire');
+
+var grandma = require('./util-grandma.js');
 
 var DATA = require('../data/testdata.js');
 
@@ -19,24 +15,10 @@ function data(name) {
     return DATA[name || 'test'].map(json).join('\n');
 }
 
-function run(command, input, done) {
-    var task = util.format('%s "%s" %s', 'node', path.join('bin', 'cli.js'), command);
-    var stream = through();
-    
-    shellton({
-        task: task,
-        cwd: root,
-        stdin: stream
-    }, done);
-    
-    stream.write(input);
-    stream.end();
-}
-
 describe('[report cli]', function() {
     describe('#default', function() {
         it('prints out text to standard out', function(done) {
-            run('report', data(), function(err, stdout, stderr) {
+            grandma('report', data(), function(err, stdout, stderr) {
                 if (err) {
                     return done(err);
                 }
@@ -51,7 +33,7 @@ describe('[report cli]', function() {
     
     describe('#text', function() {
         it('prints out text to standard out', function(done) {
-            run('report --type text', data(), function(err, stdout, stderr) {
+            grandma('report --type text', data(), function(err, stdout, stderr) {
                 if (err) {
                     return done(err);
                 }
@@ -66,7 +48,7 @@ describe('[report cli]', function() {
     
     describe('#json', function() {
         it('prints out json to standard out', function(done) {
-            run('report --type json', data(), function(err, stdout, stderr) {
+            grandma('report --type json', data(), function(err, stdout, stderr) {
                 if (err) {
                     return done(err);
                 }
