597701a4a457efb8754d5a03f0a6bd6a58d99036
Kiril Vatev <vatev.1@gmail.com>
2017-01-15 11:36:32 -0500

    updating grandma test util to provide the node binary in the path

--- test/cli/util-grandma.js
+++ test/cli/util-grandma.js
@@ -6,15 +6,16 @@ var through = require('through2');
 var root = require('rootrequire');
 
 module.exports = function run(command, input, done) {
-    // TODO figure out how to use process.execPath instead of relying
-    // on `node` to be in the path already
     var task = util.format('%s "%s" %s', 'node', path.join('bin', 'cli.js'), command);
     var stream = through();
     
     shellton({
         task: task,
         cwd: root,
-        stdin: stream
+        stdin: stream,
+        env: {
+            PATH: path.dirname(process.execPath)
+        }
     }, done);
     
     stream.write(input);
