06e9a6a82a2510275e685078fb8bd37f49bc99bb
Kiril Vatev <vatev.1@gmail.com>
2017-05-29 10:51:34 -0400

    metadata section will only show if there is content

---
+++ views/html-report/metadata.js
@@ -0,0 +1,16 @@
+/* eslint-env browser */
+
+window.addEventListener('load', function() {
+    var meta = document.querySelector('#metadata-section pre');
+    var trigger = document.querySelector('[x-for=metadata-section]');
+    
+    if (!meta || !trigger) {
+        return;
+    }
+    
+    var text = meta.innerHTML;
+
+    if (text === '') {
+        trigger.classList.add('hide');
+    }
+});
