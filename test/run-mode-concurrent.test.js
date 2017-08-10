4c6b960d4f16352aeaa9235dcb567044aaf028aa
Kiril Vatev <vatev.1@gmail.com>
2017-08-09 20:25:28 -0400

    moving the last of the run mode options to the runner api

--- test/run-mode-concurrent.test.js
+++ test/run-mode-concurrent.test.js
@@ -14,6 +14,7 @@ var rmc = function(opts) {
     var api = opts.repeater;
     api.debug = opts.debug;
     api.writeOutput = opts.writeOutput;
+    api.options = opts.options;
     
     api.on('task:run:internal', function(context) {
         opts.runTest(context);
