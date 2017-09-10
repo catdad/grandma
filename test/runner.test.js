930cbca64f4553f91ffc90b11cf163790a65cabf
Kiril Vatev <vatev.1@gmail.com>
2017-09-10 17:21:37 -0400

    adding some tests

--- test/runner.test.js
+++ test/runner.test.js
@@ -1,23 +1,84 @@
 /* eslint-env mocha */
 
 var expect = require('chai').expect;
+var sinon = require('sinon');
 
 var test = require('./_util/timeTest.js');
 
-function sharedTests() {
+var lib = require('../lib/runner.js');
+
+function sharedTests(getOpts) {
     test('does not run anything until _start is called');
 }
 
 describe('[runner]', function() {
     describe('concurrent mode', function() {
-        test('can be called in concurrnet mode');
+        function getOpts() {
+            return {
+                debug: sinon.spy(),
+                writeOutput: sinon.spy(),
+                mode: 'concurrent',
+                options: {
+                    concurrent: 5,
+                    duration: 1000
+                }
+            };
+        }
 
-        sharedTests();
+        test('can be called in concurrnet mode', function(clock) {
+            var opts = getOpts();
+            var api = lib(opts);
+            var context = {};
+            var count = 0;
+
+            function onRun(ev) {
+                expect(ev).to.deep.equal({ context: context });
+                count += 1;
+            }
+
+            api.on(api.EVENTS.RUN, onRun);
+
+            api._start({}, sinon.spy());
+            clock.tick(opts.options.duration);
+
+            expect(count).to.equal(opts.options.concurrent);
+        });
+
+        sharedTests(getOpts);
     });
 
     describe('rate mode', function() {
-        test('can be called in rate mode');
+        function getOpts() {
+            return {
+                debug: sinon.spy(),
+                writeOutput: sinon.spy(),
+                mode: 'rate',
+                options: {
+                    rate: 10,
+                    duration: 1000
+                }
+            };
+        }
 
-        sharedTests();
+        test('can be called in rate mode', function(clock) {
+            var opts = getOpts();
+            var api = lib(opts);
+            var context = {};
+            var count = 0;
+
+            function onRun(ev) {
+                expect(ev).to.deep.equal({ context: context });
+                count += 1;
+            }
+
+            api.on(api.EVENTS.RUN, onRun);
+
+            api._start({}, sinon.spy());
+            clock.tick(opts.options.duration);
+
+            expect(count).to.equal(opts.options.rate);
+        });
+
+        sharedTests(getOpts);
     });
 });
