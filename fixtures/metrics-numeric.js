3d07410cdacccb712f1b13ff609ac91b8984bad4
Kiril Vatev <vatev.1@gmail.com>
2019-09-03 21:32:02 -0400

    updating file to include timer metrics, numeric metrics, and categories

--- fixtures/metrics-numeric.js
+++ fixtures/metrics-numeric.js
@@ -2,12 +2,24 @@ function rnd(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+var i = 0;
+var categories = ['a', 'b'];
+
 module.exports = {
     test: function(done) {
-        this.metric('one', rnd(5, 10));
-        this.metric('two', rnd(300, 500));
-        this.metric('three', rnd(30, 100));
+        var that = this;
 
-        process.nextTick(done);
+        this.category(categories[(i++) % categories.length]);
+
+        that.metric('one', rnd(5, 10));
+        that.metric('two', rnd(300, 500));
+        that.metric('three', rnd(30, 100));
+
+        that.start('four');
+
+        setTimeout(function() {
+            that.end('four');
+            done();
+        }, rnd(1, 5));
     }
 };
